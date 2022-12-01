c3a3b63551dcbcea1a6df91e3161180172091f31
Sebastian-github-2020 <zaksvisitor@gmail.com>
2022-12-01 23:24:19 +0800

    添加跨域支持

--- apilearning/Controllers/AccountController.cs
+++ apilearning/Controllers/AccountController.cs
@@ -3,21 +3,23 @@ using apilearning.MyDbContext;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 // home
-namespace apilearning.Controllers {
+namespace apilearning.Controllers
+{
     /// <summary>
     /// 账户api
     /// </summary>
     [ApiExplorerSettings(GroupName = "v1")]
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountController : ControllerBase {
-        public accountContext _db { get; } = null!;
+    public class AccountController : ControllerBase
+    {
+        private accountContext Db { get; }
         /// <summary>
         /// 注入 数据库上下文
         /// </summary>
         /// <param name="db">数据库上下文</param>
         public AccountController(accountContext db) {
-            _db = db;
+            Db = db;
         }
 
         /// <summary>
@@ -26,7 +28,7 @@ namespace apilearning.Controllers {
         /// <returns></returns>
         [HttpGet("accounts")]
         public async Task<ActionResult<List<MyAccount>>> GetAccount() {
-            return await _db.MyAccounts.ToListAsync();
+            return await Db.MyAccounts.ToListAsync();
         }
 
         /// <summary>
@@ -36,9 +38,9 @@ namespace apilearning.Controllers {
         /// <returns></returns>
         [HttpPost("add")]
         public async Task<ActionResult<MyAccount>> AddAccount(MyAccount account) {
-            _db.MyAccounts.Add(account);
-            await _db.SaveChangesAsync();
-            return CreatedAtAction("account", new { id = account.Id }, account);
+            Db.MyAccounts.Add(account);
+            await Db.SaveChangesAsync();
+            return CreatedAtAction("AddAccount", new { id = account.Id }, account);
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+### 项目环境
+1. 前端Balazor server
+2. 后端webapi
+3. Share项目 共享两者的共同文件，如DTO文件(后端返回给前端的模型)
